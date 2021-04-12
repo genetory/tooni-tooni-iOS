6e2795bb4350e6dad345de75215810c03c5631ad
jinShine <seungjin429@naver.com>
2021-04-13 00:22:24 +0900

    - TabBarController 연결(Home, WebToonList, Favorite, Setting)

---
+++ toonitooni/Modules/Favorite/FavoriteViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FavoriteViewController.swift
+//  toonitooni
+//
+//  Created by buzz on 2021/04/12.
+//
+
+import UIKit
+
+class FavoriteViewController: BaseViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    print(self)
+  }
+}
