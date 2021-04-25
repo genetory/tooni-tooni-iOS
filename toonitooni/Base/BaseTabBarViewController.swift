ac4b7dc3d3e17fdd8699c43dc18fd998eb8a25dd
jinShine <seungjin429@naver.com>
2021-04-25 23:54:48 +0900

    - General navigation view 구현

---
+++ toonitooni/Base/BaseTabBarViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BaseTabBarViewController.swift
+//  toonitooni
+//
+//  Created by buzz on 2021/04/25.
+//
+
+import UIKit
+import AVKit
+
+class BaseTabBarViewController: UITabBarController {
+
+  // MARK: - Vars
+
+  // MARK: - Life Cycle
+}
